21fa402a6706f4ac1987cb30e2994d1f3b8e8efa
rookie-leo <proleonardopa@gmail.com>
2023-11-27 20:05:20 -0300

    [ build ]Usando função callback

--- src/main/java/com/example/strproducer/services/StringProducerService.java
+++ src/main/java/com/example/strproducer/services/StringProducerService.java
@@ -1,17 +1,35 @@
 package com.example.strproducer.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CompletableFuture;
+
 @Service
+@Log4j2
 @RequiredArgsConstructor
 public class StringProducerService {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("str-topic", message);
+        CompletableFuture<SendResult<String, String>> completableFuture = kafkaTemplate.send("str-topic", message);
+
+        completableFuture.whenComplete((result, ex) -> {
+            if (ex != null) {
+                log.error("Error sending message: {}", ex.getMessage());
+                return;
+            }
+            log.info("Message sent successfully: {}", result.getProducerRecord().value());
+
+            log.info("Partition {}, offset {}",
+                    result.getRecordMetadata().partition(),
+                    result.getRecordMetadata().offset());
+        });
     }
 
 }
